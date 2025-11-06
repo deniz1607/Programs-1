83bd549f316c6d143b0e67c6f5f464460c2e29bb
ADIEN24 <asya.vangelova.2026@spgke.com>
2025-11-06 08:39:33 +0200

    .

---
+++ Program2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+public class InventoryUpdate
+{
+    public static void Main(string[] args)
+    {
+        int inventory = 500;
+        int itemsSold = 75;
+        int invoiceNumber = 2024001;
+        int lateDays = 0;
+
+        //Съкратен запис за изваждане (Продажба)
+        inventory -= itemsSold; // inventory = 500 - 75; => 425
+        Console.WriteLine($"Оставаща наличност:" + inventory);
+
+        //Съкратен запис за умножение (Месечно натрупване)  
+        lateDays = 1; 
+        lateDays *= 3; //Натрупване на забава от 3 месеца
+        Console.WriteLine($"Натрупани дни забавяне: " + lateDays);
+
+        //Инкрементиране (Следваща фактура)
+        invoiceNumber++; //invoiceNumber = 2024002
+        Console.WriteLine($"Следващ номер на фактура: " + invoiceNumber);
+    }
+}        
